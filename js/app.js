9f05bac4fe7725a3e07321898accc490f615c470
Master <joelpalacio630@gmail.com>
2023-10-07 23:36:19 -0600

    feat: creando y validando la funcionalidad del boton Reset

--- js/app.js
+++ js/app.js
@@ -6,11 +6,24 @@ document.addEventListener('DOMContentLoaded',function () {
     const inputMensaje = document.querySelector('#mensaje');
     const formulario = document.querySelector('#formulario');
     const btnSubmit = document.querySelector('#formulario button[type="submit"]');
+    const btnReset = document.querySelector('#formulario button[type="reset"]');
+
 
     // Asignar eventos
     inputEmail.addEventListener('input',validar)
     inputAsunto.addEventListener('input',validar)
     inputMensaje.addEventListener('input',validar);
+    btnReset.addEventListener('click',function (e) {
+        e.preventDefault();
+
+        // Reiniciar el objeto
+        email.email = '';
+        email.asunto = '';
+        email.mensaje = '';
+
+        formulario.reset();
+        comprobarEmail();
+    })
 
     // Objeto que contiene los campos del formulario
     const email = {
